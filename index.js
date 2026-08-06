e302d0389af67ba0755ca7bd787a1e1e4153d6bc
caothitunguyet-prog <caothitunguyet@gmail.com>
2026-08-06 15:13:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ http.createServer((req, res) => res.end('Bot AFK Alive')).listen(process.env.POR
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: 'ĐIỀN_DYNIP_VỪA_COPY', // Ví dụ: 'xxxx.aternos.host'
-    port: 12345,                // Điền số Port 5 chữ số vừa copy ở trên (không dùng 17086)
+    host: 'serverthunghiem-FT6U.aternos.me', // Ví dụ: 'xxxx.aternos.host'
+    port: 17086,                // Điền số Port 5 chữ số vừa copy ở trên (không dùng 17086)
     username: 'Bot_AFK_247',
     version: false
   });
